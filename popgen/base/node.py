ec6109520a4617a8593b0a6f8e7cfd0976dc808d
Haotian <zhang@haotian.info>
2021-12-02 00:33:38 -0500

    0.0.0

--- popgen/base/node.py
+++ popgen/base/node.py
@@ -1,6 +1,21 @@
 from collections import OrderedDict
+from uuid import uuid1
 
 class Node(object):
     def __init__(self, name=None, identifier=None):
         self.name = name
         self.identifier = None
+        if name is None:
+            self.name = self.identifier
+
+    @property
+    def identifier(self):
+        return self.identifier
+
+    @identifier.setter
+    def identifier(self, nid):
+        _identifier = nid
+        if nid is None:
+            _identifier = str(uuid1())
+        self.identifier = _identifier
+
--- popgen/base/tree.py
+++ popgen/base/tree.py
@@ -3,6 +3,19 @@ class BaseTree(object):
         # some init settings
         pass
 
+    def __contains__(self, identifier):
+        # if a node is in this tree
+        pass
+    
+    def __getitem__(self, key):
+        # get a node from node list
+        pass
+
+    def __len__(self):
+        # obtain number of nodes in a tree
+        pass
+
+
     def from_newick(self, newick):
         # construct a tree from newick format string
         pass
