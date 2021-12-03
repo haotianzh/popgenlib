66d4a9e45543179ebbc807c5dfccd12496d8731c
Haotian <zhang@haotian.info>
2021-12-03 12:19:11 -0500

    0.0.0

--- popgen/base/node.py
+++ popgen/base/node.py
@@ -1,21 +1,56 @@
 from collections import OrderedDict
 from uuid import uuid1
+from copy import deepcopy
+
 
 class Node(object):
     def __init__(self, name=None, identifier=None):
-        self.name = name
-        self.identifier = None
+        self.identifier = identifier
         if name is None:
             self.name = self.identifier
+        else:
+            self.name = name
+        self.parent = {}
+        self.children = OrderedDict()
+        self.childs = []
+
+
+    def add_child(self, node):
+        if node in self.children:
+            raise Exception('node has already been added.')
+        if node in self.parent:
+            raise Exception('parent cannot be added as a child.')
+        self.children[node.identifier] = node
+        self.childs.append(node)
+
+    def set_parent(self, parent):
+        if parent is None:
+            self.parent = {}
+        else:
+            self.parent[parent.identifier] = parent
 
     @property
     def identifier(self):
-        return self.identifier
+        return self._identifier
 
     @identifier.setter
     def identifier(self, nid):
-        _identifier = nid
         if nid is None:
-            _identifier = str(uuid1())
-        self.identifier = _identifier
+            nid = str(uuid1())
+        self._identifier = nid
 
+    @property
+    def level(self):
+        return self._level
+
+    @level.setter
+    def level(self, value):
+        if not isinstance(value, int):
+            raise Exception('value of level should be an integer.')
+        if value < 0:
+            raise Exception('value cannot be negative.')
+        self._level = value
+
+if __name__ == '__main__':
+    node = Node()
+    print(node.identifier, node.name)
--- popgen/base/tree.py
+++ popgen/base/tree.py
@@ -1,24 +1,60 @@
+from collections import OrderedDict
+from .node import Node
+
 class BaseTree(object):
     def __init__(self):
-        # some init settings
-        pass
+        self._root = None
+        self._nodes = OrderedDict()
+        self._levels = 0
 
-    def __contains__(self, identifier):
+    def __contains__(self, node):
         # if a node is in this tree
-        pass
+        if isinstance(node, Node):
+            node = node.identifier
+        if node in self._nodes:
+            return True
+        else:
+            return False
     
-    def __getitem__(self, key):
+    def __getitem__(self, identifier):
         # get a node from node list
-        pass
+        return self._nodes[identifier]
 
     def __len__(self):
         # obtain number of nodes in a tree
-        pass
-
+        return len(self._nodes)
 
     def from_newick(self, newick):
         # construct a tree from newick format string
         pass
+
+    def create_node(self, name=None, identifier=None, parent=None):
+        node = Node(name=name, identifier=identifier)
+        self.add_node(node, parent)
+
+    def add_node(self, node, parent=None):
+        if node.identifier in self._nodes:
+            raise Exception('cannot add the node that has already been in the tree.')
+        if parent is None:
+            if self._root:
+                raise Exception('root has already existed and parent cannot be none.')
+            self._root = node
+            self._nodes[node.identifier] = node
+            node.set_parent(None)
+            return
+        pid = parent.identifier if isinstance(parent, Node) else parent
+        if not pid in self._nodes:
+            raise Exception('parent not found in this tree.')
+        self._nodes[node.identifier] = node
+        node.set_parent(self[pid])
+        self[pid].add_child(node)
+        return
+
+    def get_all_nodes(self, func=None):
+        if func is None:
+            return self._nodes
+        return func(self._nodes)
+
 
 
 
